2b07541b3f5c76d9ca353dc7401b6ec84fd4911d
Gustavo Martins Oliveira <gmocheats@hotmail.com>
2021-03-25 16:32:24 -0300

    Publisher e consumer

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Azure.Messaging.EventHubs.Processor;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Consumer
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string ehubNamespaceConnectionString = "";
+        private const string eventHubName = "";
+        private const string blobStorageConnectionString = "";
+        private const string blobContainerName = "";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Read from the default consumer group: $Default
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                eventHubName,
+                PartitionReceiver.DefaultConsumerGroupName,
+                ehubNamespaceConnectionString,
+                blobStorageConnectionString,
+                blobContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<EventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+            Console.ReadKey();
         }
     }
 }
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
 
 namespace Publisher
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string connectionString = "";
+        private const string eventHubName = "";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            EventHubProducerClient producerClient = new EventHubProducerClient(connectionString, eventHubName);
+
+            // Create a batch of events 
+            using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+            // Add events to the batch. An event is a represented by a collection of bytes and metadata. 
+            eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("First event")));
+            eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Second event")));
+            eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Third event")));
+
+            // Use the producer client to send the batch of events to the event hub
+            await producerClient.SendAsync(eventBatch);
+            Console.WriteLine("A batch of 3 events has been published.");
+            Console.ReadKey();
+
         }
     }
 }
